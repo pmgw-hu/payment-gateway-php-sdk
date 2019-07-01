d6e11d91835beb1abe7a949b4648c9d7df4f5e8c
Túri Bence <bence.turi@bigfish.hu>
2019-07-01 13:34:40 +0200

    sca - details
    Signed-off-by: Túri Bence <bence.turi@bigfish.hu>

--- src/BigFish/PaymentGateway/Request/PaymentLinkDetails.php
+++ src/BigFish/PaymentGateway/Request/PaymentLinkDetails.php
@@ -25,14 +25,23 @@ class PaymentLinkDetails extends RequestAbstract
 	public $paymentLinkName;
 
 	/**
-	 * Construct payment link details request instance
-	 * 
-	 * @param string $paymentLinkName Payment Link Name received from Payment Gateway
-	 * @return void
+	 * Get related Info data
+	 *
+	 * @var boolean
 	 * @access public
 	 */
-	public function __construct($paymentLinkName)
+	public $getInfoData;
+
+	/**
+	 * Construct payment link details request instance
+	 *
+	 * @param string $paymentLinkName Payment Link Name received from Payment Gateway
+	 * @param bool $getInfoData Get related Info data (true/false)
+	 * @access public
+	 */
+	public function __construct($paymentLinkName, $getInfoData = false)
 	{
 		$this->paymentLinkName = $paymentLinkName;
+		$this->getInfoData = (($getInfoData === true || $getInfoData == "true") ? true : false);
 	}
 }
