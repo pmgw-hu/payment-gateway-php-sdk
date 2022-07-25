68b7c56d69e19112e697ae74006cd5df0e5ba48c
Richer Csaba <csaba.richer@bigfish.hu>
2022-07-25 10:55:12 +0200

    727 - paywall: added paywall payment details

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Version
 
-3.14.0
+3.15.0
 
 ## Requirements
 
---
+++ src/BigFish/PaymentGateway/Request/PayWallPaymentDetails.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * BIG FISH Payment Gateway (https://www.paymentgateway.hu)
+ * PHP SDK
+ *
+ * @link https://github.com/bigfish-hu/payment-gateway-php-sdk.git
+ * @copyright (c) 2015, BIG FISH Internet-technology Ltd. (http://bigfish.hu)
+ */
+namespace BigFish\PaymentGateway\Request;
+
+/**
+ * PayWall payment details request class
+ *
+ * @package PaymentGateway
+ * @subpackage Request
+ */
+class PayWallPaymentDetails extends RequestAbstract
+{
+	/**
+	 * PayWall payment identifier
+	 *
+	 * @var string
+	 * @access public
+	 */
+	public $paywallPaymentName;
+
+	/**
+	 * GetTransactions
+	 *
+	 * @var boolean
+	 * @access public
+	 */
+	public $getTransactions;
+
+	/**
+	 * Construct new PayWall payment details request instance
+	 *
+	 * @param string $paywallPaymentName PayWall identifier received from Payment Gateway
+	 * @return void
+	 * @access public
+	 */
+	public function __construct($paywallPaymentName)
+	{
+		$this->paywallPaymentName = $paywallPaymentName;
+	}
+
+	/**
+	 * Set if Details response should include transactions data
+	 *
+	 * @param bool $getTransactions getTransactions flag
+	 * @return \BigFish\PaymentGateway\Request\PayWallPaymentDetails
+	 * @access public
+	 */
+	public function setGetTransactions($getTransactions)
+	{
+		$this->getTransactions = $getTransactions;
+		return $this;
+	}
+}
